3478a73a82af8530d6ba038533d2c18b098d7728
Dlord250 <devidgame981@gmail.com>
2023-12-02 17:03:09 -0300

    update

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const pesquisa = document.querySelector('search')
+
+pesquisa.addEventListener('click', () => {
+    pesquisa.classList.add('.pesqIn')});
---
+++ src/js/scroll.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.pesquisaTxt2',{
+    duration: 2000
+});
